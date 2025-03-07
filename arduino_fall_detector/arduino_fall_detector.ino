4bd911a5af7aed2756a48fcb24378a814858a22c
Ramoj naidu <143724619+Ramojinaidu@users.noreply.github.com>
2025-03-07 18:27:28 +0530

    Add files via upload

--- arduino_fall_detector/arduino_fall_detector.ino
+++ arduino_fall_detector/arduino_fall_detector.ino
@@ -1,11 +1,33 @@
+/*
+///////////////////////////////////////////////
+**********COMMUNITY SERVICE PROJECT************
+    
+      project_name      : Fall-detection Using arduino and esp32
+      project_guide     :
+      project_members   :         Name                                   Roll-Number
+                          1)
+                          2)
+                          3)
+                          4)
+                          5)
+
+      branch            : Electronics And Communication Engineering
+      course            : B-Tech
+      year              : 
+
+///////////////////////////////////////////////////
+*/
+
+
+
 #include <SoftwareSerial.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 
-SoftwareSerial mySerial1(10, 11);           // Set up a new SoftwareSerial object with RX in digital pin 10 and TX in digital pin 11
-SoftwareSerial mySerial2(3,2);              // Set up a new SoftwareSerial object with RX in digital pin 03 and TX in digital pin 02
+SoftwareSerial mySerial1(10, 11);           // Set up a new SoftwareSerial object with RX in digital pin 10 and TX in digital pin 11 for esp32
+SoftwareSerial mySerial2(3,2);              // Set up a new SoftwareSerial object with RX in digital pin 03 and TX in digital pin 02 for GSM module
 
 const float fallThreshold = 2.0; 
 const float gyroThreshold = 5.0; 
@@ -22,41 +44,58 @@ String phone_number = "+919391259921";        // Enter the recipient's phone num
 float sensor_data[6];
 
 void setup() {
+                                              //Set the baud rate for the SerialSoftware objects  
+                                              //    Device               Baud-rate
+  mySerial1.begin(9600);                      //     Esp32           -       9600                  
+  mySerial2.begin(115200);                    //     GSM             -       115200
+  Serial.begin(115200);                       //     Seril-monitor   -       115200
                                               
-  mySerial1.begin(9600);                   // Set the baud rate for the SerialSoftware object
-  mySerial2.begin(115200);
-  Serial.begin(115200); 
-  while(!mpu.begin()) Serial.println("mpu6050 not connected");
+  while(!mpu.begin())
+     Serial.println("mpu6050 not connected"); //checking for mpu6050 connection
 
 }
 
 void loop() {
 
 
-  mpu.getEvent(&accel, &gyro, &temp);
-  sensor_data[0] = accel.acceleration.x;
+  mpu.getEvent(&accel, &gyro, &temp);         // getting mpu6050 values
+
+
+  // loading mpu6050 values
+  sensor_data[0] = accel.acceleration.x;     
   sensor_data[1] = accel.acceleration.y;
   sensor_data[2] = accel.acceleration.z; 
   sensor_data[3] = gyro.gyro.x;
   sensor_data[4] = gyro.gyro.y;
   sensor_data[5] = gyro.gyro.z;
 
+  // caleculating resultant accelration and resultant gyro
   float totalAcceleration = sqrt(pow(accel.acceleration.x, 2) + pow(accel.acceleration.y, 2) + pow(accel.acceleration.z, 2));
   float totalGyro = sqrt(pow(gyro.gyro.x, 2) + pow(gyro.gyro.y, 2) + pow(gyro.gyro.z, 2));
-  Serial.println(totalGyro);
+
+
 
   if (totalAcceleration > fallThreshold && totalGyro > gyroThreshold) {
 
 
-    message = "Acceleration values: \n  X = " + String(sensor_data[0], 2) + " \n Y = " + String(sensor_data[1], 2) + "  \n Z = " + String(sensor_data[2], 2) + "\n Gyro values: \n X = " + String(sensor_data[3], 2) + " \n Y = " + String(sensor_data[4], 2) + "  \n Z = " + String(sensor_data[5], 2);
+    message = "Acceleration values: \n \
+    X = " + String(sensor_data[0], 2) + " \n \
+    Y = " + String(sensor_data[1], 2) + "  \n \
+    Z = " + String(sensor_data[2], 2) + "\n \
+    Gyro values: \n \
+    X = " + String(sensor_data[3], 2) + " \n \
+    Y = " + String(sensor_data[4], 2) + "  \n \
+    Z = " + String(sensor_data[5], 2) + "\n \
+    status: Fall Detected!!";
+
+
+    
     if (mySerial1.isListening() || mySerial2.isListening()){
       delay(200);
       send_message(message);
       delay(200);
       send_call();
 
-
-      
     }
 
 
@@ -81,7 +120,7 @@ void senddata(){
 void send_message(String message ){
 
   mySerial2.println("AT+CMGF=1");
-  delay(200);                                // Configuring TEXT mode
+  delay(200);                                        // Configuring TEXT mode
   mySerial2.println("AT+CMGS=\"" + phone_number + "\"");   
     delay(200);      
   mySerial2.print(message);
@@ -96,7 +135,6 @@ void send_call(){
   mySerial2.println("AT+CMGF=0");                                // Configuring TEXT mode
   delay(200);
   mySerial2.println("ATD" + phone_number + ";"); 
-    Serial.println("ATD" + phone_number + ";");
 
 }
 
